ba9f85da892c888a15c0ed27ec336560f4f562b4
Lucas Losano <losano3@hotmail.com>
2024-02-07 19:52:18 -0300

    Bluetooth connection

---
+++ LuvaApp/LuvaApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LuvaApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LuvaApp/LuvaApp/Interfaces/IBluetoothController.cs
@@ -0,0 +1,18 @@
+﻿using Plugin.BLE.Abstractions.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuvaApp.Interfaces
+{
+    internal interface IBluetoothController
+    {
+        public IDevice? ConnectedDevice { get; set; }
+        public Task<IEnumerable<IDevice>> AsyncGetDevices();
+        public Task AsyncConnectToDeviceByName(string deviceName);
+        public void DisconnectFromDevice();
+        public Task AsyncRequestBluetoothPermissions();
+    }
+}
