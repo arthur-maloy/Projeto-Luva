d8ab442284ecb38ebe7ea3a834de7586300fd286
RafaelPignatari <rafaelpignatarifantinati@hotmail.com>
2024-02-11 13:17:15 -0300

    Adição de mecanismo de Recepção ao aplicativo

---
+++ Software/LuvaApp/LuvaApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LuvaApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Software/LuvaApp/LuvaApp/Interfaces/IPrediction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuvaApp.Interfaces
+{
+    internal interface IPrediction
+    {
+        public string Predict(object[] sensorValues);
+    }
+}
