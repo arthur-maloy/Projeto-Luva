f63e9513994155970790c0cbafbcf2c5d66fec33
Rafael Pignatari <rafaelpignatarifantinati@hotmail.com>
2024-02-11 22:17:38 -0300

    Criação de mecanismo de atualização automática e otimização de IA.

--- Software/LuvaApp/LuvaApp/Helpers/IAEmbarcadaController.cs
+++ Software/LuvaApp/LuvaApp/Helpers/IAEmbarcadaController.cs
@@ -5,7 +5,34 @@ namespace LuvaApp.Helpers
 {
     public class IAEmbarcadaController
     {
-        private async static Task<PredictionEngine<OnnxInput, OnnxOutput>> GetPredictionPipeline(MLContext mLContext)
+        private PredictionEngine<OnnxInput, OnnxOutput> _predictionEngine;
+
+        private IAEmbarcadaController()
+        {
+            MLContext mlContext = new MLContext();
+            Task.Run(async() => _predictionEngine = await GetPredictionPipeline(mlContext));
+        }
+
+        #region SINGLETON
+        private static IAEmbarcadaController _instancia;
+
+        // Private constructor to prevent instance creation
+
+        public static IAEmbarcadaController Instancia
+        {
+            get
+            {
+                if (_instancia == null)
+                {
+                    _instancia = new IAEmbarcadaController();
+                }
+
+                return _instancia;
+            }
+        }
+        #endregion
+
+        private async Task<PredictionEngine<OnnxInput, OnnxOutput>> GetPredictionPipeline(MLContext mLContext)
         {
             var inputColumns = new string[] { "float_input" };
             var outputColumns = new string[] { "label", "probabilities" };
@@ -22,14 +49,22 @@ namespace LuvaApp.Helpers
             return mLContext.Model.CreatePredictionEngine<OnnxInput, OnnxOutput>(model);
         }
 
-        public async static Task<string> Predicao(OnnxInput entrada)
+        public async Task<long> Predicao(OnnxInput entrada)
         {
-            MLContext mlContext = new MLContext();
+            try
+            {
+                if (_predictionEngine == null)
+                    return -1;
 
-            var predEngine = await GetPredictionPipeline(mlContext);
-            var result = predEngine.Predict(entrada);
+                var result = _predictionEngine.Predict(entrada);
 
-            return result.ToString();
+                return result.Resultado[0];
+            }
+            catch (Exception ex)
+            {
+                //TODO: Adicionar logs
+                return -1;
+            }
         }
 
     }
--- Software/LuvaApp/LuvaApp/MainPage.xaml.cs
+++ Software/LuvaApp/LuvaApp/MainPage.xaml.cs
@@ -1,16 +1,21 @@
 ﻿using LuvaApp.Helpers.BluetoothHelper;
 using LuvaApp.Helpers;
 using LuvaApp.Models;
+using LuvaApp.ViewModels;
 
 namespace LuvaApp
 {
     public partial class MainPage : ContentPage
     {
         BluetoothController bluetoothController = new BluetoothController();
+        PosicaoViewModel _posicaoViewModel;
 
         public MainPage()
         {
             InitializeComponent();
+
+            _posicaoViewModel = new PosicaoViewModel();
+            BindingContext = _posicaoViewModel;
         }
 
         private async void ConnectToBluetoothDevice(object sender, EventArgs e)
@@ -29,12 +34,37 @@ namespace LuvaApp
 
         private void TraduzSinalBtn_Clicked(object sender, EventArgs e)
         {
-            var input = new OnnxInput
-            {
-                Sensores = new float[] { 1646f, -31f, -107f },
-            };
+            TraduzSinalBtn.IsEnabled = false;
+            TraduzSinalBtn.Text = "Atualizando automaticamente predict";
 
-            var test = IAEmbarcadaController.Predicao(input);
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var recebido = RecepcaoController.Instancia.ObtemUltimoValorRecebido();
+
+                        var input = new OnnxInput
+                        {
+                            Sensores = new float[] { recebido.Flexao2, recebido.Acc_EixoX, recebido.Acc_EixoY },
+                        };
+
+                        Task.Run(async () =>
+                        {
+                            var predict = await IAEmbarcadaController.Instancia.Predicao(input);
+
+                            await MainThread.InvokeOnMainThreadAsync(() => _posicaoViewModel.Posicao = predict);
+                        });
+                    }
+                    catch (Exception ex)
+                    {
+                       //TODO: Adicionar logs
+                    }
+
+                    Thread.Sleep(50);
+                }
+            });
         }
     }
 }
